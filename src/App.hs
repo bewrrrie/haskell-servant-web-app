abf724cd7905a0297c369bc21bfea33f6aa669c6
bewrrrie <vadim.leshkov@proton.me>
2022-09-04 22:48:34 +0700

    Add request body to POST endpoint.

--- src/App.hs
+++ src/App.hs
@@ -1,22 +1,38 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module App
   ( run
   ) where
 
 import Control.Monad.Trans
+import Data.Aeson
 import qualified Data.Text as T
+import GHC.Generics (Generic)
 import Grading
 import qualified Network.Wai.Handler.Warp as W
 import Servant
 
+data SubmitInfo =
+  SubmitInfo
+    { text :: T.Text
+    , userId :: T.Text
+    }
+  deriving (Generic, Show, Eq)
+
+instance ToJSON SubmitInfo
+
+instance FromJSON SubmitInfo
+
 type TextReviewAPI
-   = "submit" :> QueryParam "text" T.Text :> Post '[ JSON] TextReview
+   = "submit" :> ReqBody '[ JSON] SubmitInfo :> Post '[ JSON] TextReview
 
 server :: Server TextReviewAPI
-server Nothing = throwError err404
-server (Just txt) = liftIO $ grade txt
+server body = liftIO $ grade txt
+  where
+    txt = text body
+    uid = userId body
 
 textReviewAPI :: Proxy TextReviewAPI
 textReviewAPI = Proxy
