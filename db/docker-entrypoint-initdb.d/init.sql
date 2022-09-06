7d0ba6a6a8047e9890879964274684626aa3ea1b
bewrrrie <vadim.leshkov@proton.me>
2022-09-06 09:48:48 +0700

    Add 'timestamp' column.

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -4,6 +4,7 @@ CREATE DATABASE appdb;
 
 CREATE TABLE submissions (
   id INT NOT NULL PRIMARY KEY,
+  datetime TIMESTAMP NOT NULL,
   name TEXT NOT NULL,
   text TEXT NOT NULL,
   score INT NOT NULL
--- src/SubmissionsDB.hs
+++ src/SubmissionsDB.hs
@@ -10,6 +10,7 @@ module SubmissionsDB
 import Contravariant.Extras.Contrazip
 import Data.ByteString.Char8 as C
 import qualified Data.Text as T
+import Data.Time.LocalTime
 import GHC.Int
 import GHC.Word
 import Hasql.Connection
@@ -33,6 +34,7 @@ createSubbmissionsTableStatement =
   let sql =
         "create table if not exists submissions ( \
         \ id serial not null primary key, \
+        \ datetime timestamp not null, \
         \ name text not null, \
         \ text text not null, \
         \ score int not null )"
@@ -50,13 +52,14 @@ runSession conn session = do
     Right result -> return result
     Left err -> error $ show err
 
-insertSubmissionStatement :: Statement (T.Text, T.Text, Int16) ()
+insertSubmissionStatement :: Statement (LocalTime, T.Text, T.Text, Int16) ()
 insertSubmissionStatement =
   let sql =
-        "insert into submissions (name, text, score) \
-        \values ($1, $2, $3)"
+        "insert into submissions (datetime, name, text, score) \
+        \values ($1, $2, $3, $4)"
       encoder =
-        contrazip3
+        contrazip4
+          (E.param (E.nonNullable E.timestamp))
           (E.param (E.nonNullable E.text))
           (E.param (E.nonNullable E.text))
           (E.param (E.nonNullable E.int2))
@@ -64,5 +67,8 @@ insertSubmissionStatement =
    in Statement sql encoder decoder True
 
 addSubmission :: Connection -> T.Text -> T.Text -> Int16 -> IO ()
-addSubmission conn name txt score =
-  runSession conn $ statement (name, txt, score) insertSubmissionStatement
+addSubmission conn name txt score = do
+  zonedTimestamp <- getZonedTime
+  let localTimestamp = zonedTimeToLocalTime zonedTimestamp
+  runSession conn $
+    statement (localTimestamp, name, txt, score) insertSubmissionStatement
