e71a40cc29494179ecd94398d53c757c854ec0e3
bewrrrie <vadim.leshkov@proton.me>
2022-09-06 11:15:40 +0700

    Debug scenario.

---
+++ run.sh
@@ -0,0 +1,5 @@
+docker network create app-network
+docker build -t "app-db" ./db
+docker run --network app-network --name app-db-container -p 5555:5432 -d app-db
+stack build
+stack exec app-exe 8080 localhost 5555 user pass
