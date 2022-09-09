fac8177678c5995ab5ea8ec68700a8cdfb651659
bewrrrie <vadim.leshkov@proton.me>
2022-09-09 23:21:46 +0700

    Upd. test request.

--- run.sh
+++ run.sh
@@ -3,4 +3,3 @@ docker build -t "app-db" ./db
 docker run --network app-network --name app-db-container -p 5555:5432 -d app-db
 stack build
 stack exec app-exe 8080 localhost 5555 user pass
-curl -i -X POST -H "Content-Type: application/json;charset=utf-8" -d '{"userName":"NAME", "text":"синхрафазатрон в дубне"}' http://localhost:8080/submit
---
+++ test.sh
@@ -0,0 +1,1 @@
+curl -i -X POST -H "Content-Type: application/json;charset=utf-8" -d '{"userName":"NAME", "text":"синхрафазатрон в дубне"}' http://localhost:8080/submit
