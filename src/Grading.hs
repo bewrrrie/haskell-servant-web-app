fec06fbf6a28e275f4190cc43fd5f7d1ed7b238c
bewrrrie <vadim.leshkov@proton.me>
2022-09-12 02:36:17 +0700

    Add doctest for grade function.

--- src/Grading.hs
+++ src/Grading.hs
@@ -41,6 +41,23 @@ misspellingsToGrade n
   | n > 5 = 0
   | otherwise = 5 - fromIntegral n
 
+-- | @grade @txt is a function which converts text @txt
+-- | to a @TextReview object that contains the list of
+-- | misspelled words and the score computed from the
+-- | number of misspellings.
+--
+-- import qualified Data.Text as T
+-- Examples:
+-- >>> grade $ T.pack ""
+-- TextReview {misspelled = [], score = 5}
+-- >>> grade $ T.pack "A catt and a dogg."
+-- TextReview {misspelled = ["catt","dogg"], score = 3}
+-- >>> grade $ T.pack "A cat and a dog."
+-- TextReview {misspelled = [], score = 5}
+-- >>> grade $ T.pack "Dont do this!"
+-- TextReview {misspelled = ["Dont"], score = 4}
+-- >>> grade $ T.pack "Don't do this!"
+-- TextReview {misspelled = [], score = 5}
 grade :: T.Text -> IO TextReview
 grade txt = misspelledWords >>= k
   where
