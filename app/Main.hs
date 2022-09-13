f887af3f210d6c04848ccfdd5ac378cfcbc5268a
bewrrrie <vadim.leshkov@proton.me>
2022-09-14 05:22:31 +0700

    Use optparse.

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,47 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import App
-import qualified Data.ByteString.Char8 as C
-import Data.Functor ((<&>))
+import Data.ByteString as B
+import GHC.Word (Word16)
 import Hasql.Connection (release)
+import Options.Applicative
 import SubmissionsDB (connectToDB, initDB)
-import System.Environment
+
+data Options =
+  Options
+    { port :: String
+    , urlDB :: B.ByteString
+    , portDB :: Word16
+    , userDB :: B.ByteString
+    , passDB :: B.ByteString
+    }
 
 main :: IO ()
 main = do
-  [port, urlDB, portDB, userDB, passDB] <- getArgs <&> take 5
-  let (_urlDb, _portDB, _userDB, _passDB) =
-        (C.pack urlDB, read portDB, C.pack userDB, C.pack passDB)
-  connection <- connectToDB _urlDb _portDB _userDB _passDB
+  Options { port = _port
+          , urlDB = _urlDB
+          , portDB = _portDB
+          , userDB = _userDB
+          , passDB = _passDB
+          } <- execParser parserInfo
+  connection <- connectToDB _urlDB _portDB _userDB _passDB
   initDB connection
-  run connection $ read port
+  run connection $ read _port
   release connection
+
+parser :: Parser Options
+parser =
+  Options <$> portOpt <*> urlDbOpt <*> portDbOpt <*> userDbOpt <*> passDbOpt
+  where
+    portOpt = argument str (metavar "PORT")
+    urlDbOpt = argument str (metavar "URL_DB")
+    portDbOpt = argument auto (metavar "PORT_DB")
+    userDbOpt = argument str (metavar "USER_DB")
+    passDbOpt = argument str (metavar "PASS_DB")
+
+parserInfo :: ParserInfo Options
+parserInfo =
+  info
+    (parser <**> helper)
+    (fullDesc <>
+     progDesc "Run spell check web service" <> header "Sample web app")
